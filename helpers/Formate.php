717318767533aaa3a59d4379cd5978de326770df
Sajib <mhsajib44@gmail.com>
2020-09-07 16:04:54 +0600

    v.1

---
+++ helpers/Formate.php
@@ -0,0 +1,22 @@
+<?php
+class Formate{
+	public function formDate($date){
+		return date('F j,Y, g:i a', strtotime($date));
+	}
+
+	public function textshorten($text,$limit=400){
+		$text=$text;
+		$text=substr($text, 0,$limit);
+		$text=substr($text, 0, strrpos($text, ' '));
+		$text=$text."...........";
+		return $text;
+	}
+
+	public function validation($data){
+	$data=trim($data);
+	$data=stripslashes($data);
+	$data=htmlspecialchars($data);
+	return $data;	
+	}
+}
+?>
